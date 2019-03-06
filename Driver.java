6403e3d1308244c4fd2f8e315104ed527af7ee55
aditihaiman <ahaiman00@stuy.edu>
2019-03-06 10:09:07 -0500

    reading in file and testing

---
+++ Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+
+  public static void main(String[] args) {
+
+    try {
+      USACO test = new USACO("makelake.in");
+    }
+    catch(Exception e) {}
+
+  }
+
+}
--- USACO.java
+++ USACO.java
@@ -1,8 +1,48 @@
+import java.io.*;
+import java.util.*;
+
 public class USACO {
 
-  // ---------------------- BRONZE PROBLEM ----------------------- //
+  public int[][] lake;
+  public int row, col, elevation;
+  public int[][] stomps;
 
-   
+  // ---------------------- BRONZE PROBLEM ----------------------- //
+  public USACO(String filename) throws FileNotFoundException {
+    File text = new File(filename);
+    Scanner inf = new Scanner(text);
+    row = inf.nextInt();
+    col = inf.nextInt();
+    elevation = inf.nextInt();
+    int temp = inf.nextInt();
+    lake = new int[row][col];
+    for(int x = 0; x < row; x++) { //builds array representing the pasture
+      for(int y = 0; y < col; y++) {
+        lake[x][y] = inf.nextInt();
+      }
+      inf.nextLine();
+    }
+    stomps = new int[temp][3]; //array with stomp digging instructions
+    for(int x = 0; x < temp; x++){
+      for(int y = 0; y < 3; y++){
+        stomps[x][y] = inf.nextInt();
+      }
+      inf.nextLine();
+    }
+  }
+
+  public static int bronze(String filename) {
+    return 0;
+  }
+
+  
+
+
+  // ------------------------ SILVER ----------------------- //
+
+
+
+
 
 
 }
