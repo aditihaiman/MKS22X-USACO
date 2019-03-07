1150cc5b8be107271a0e89576472aaaf4a124cfc
aditihaiman <ahaiman00@stuy.edu>
2019-03-07 10:13:20 -0500

    reading in file

--- Driver.java
+++ Driver.java
@@ -5,21 +5,24 @@ public class Driver {
 
   public static void main(String[] args) {
 
-    try {
+    try { //BRONZE
       USACO test = new USACO("makelake.in");
-
-    int[] dig = {1, 4, 4};
-    test.stompDig(dig);
-
-    for(int x = 0; x < test.lake.length; x++) {
-      System.out.println(Arrays.toString(test.lake[x]));
-    }
-
-    System.out.println(USACO.bronze("makelake.in"));
+      int[] dig = {1, 4, 4};
+      test.stompDig(dig);
+      for(int x = 0; x < test.lake.length; x++) {
+        System.out.println(Arrays.toString(test.lake[x]));
+      }
+      System.out.println(USACO.bronze("makelake.in"));
 
     }
     catch(Exception e) {}
 
+
+    try{ //SILVER
+      test = new USACO1("ctravel.in");
+
+    }
+    catch(Exception e){}
 
   }
 
--- USACO.java
+++ USACO.java
@@ -35,9 +35,9 @@ public class USACO {
     USACO solve = new USACO(filename);
     for(int x = 0; x < solve.stomps.length; x++){
       solve.stompDig(solve.stomps[x]);
-    }
+    } //by this point, all stomp digging is finished
     int sum = 0;
-    for(int x = 0; x < solve.row; x++) {
+    for(int x = 0; x < solve.row; x++) { //adds up elevations in the lake
       for(int y = 0; y < solve.col; y++) {
         if( solve.elevation - solve.lake[x][y] > 0) sum+= solve.elevation - solve.lake[x][y];
       }
@@ -48,14 +48,14 @@ public class USACO {
   public void stompDig(int[] ins) {
     System.out.println(Arrays.toString(ins));
     int max = 0;
-    for(int x = ins[0] - 1; x < ins[0]+2; x++) {
+    for(int x = ins[0] - 1; x < ins[0]+2; x++) { //finds the largest value within stompdigging area
       for(int y = ins[1] - 1; y < ins[1]+2; y++) {
         if (lake[x][y] >= max) {
           max = lake[x][y];
         }
       }
     }
-    max = max - ins[2];
+    max = max - ins[2]; //replacing all proper values with the new elevation
     for(int x = ins[0] - 1; x < ins[0]+2; x++) {
       for(int y = ins[1] - 1; y < ins[1]+2; y++) {
         if (lake[x][y] > max) lake[x][y] = max;
@@ -70,22 +70,31 @@ public class USACO {
   public int row1, col1, sec;
   public int[] start, end;
 
-  public void USACO1(String filename) throws FileNotFoundException{
+  public void readFile(String filename) throws FileNotFoundException{
     File text = new File(filename);
     Scanner inf = new Scanner(text);
     row1 = inf.nextInt();
     col1 = inf.nextInt();
     sec = inf.nextInt();
+    field = new int[row1][col1];
     for(int x = 0; x < row1; x++){
       for(int y = 0; y < col1; y++){
-
+        field[x][y] = inf.nextInt();
       }
+      inf.nextLine();
     }
+    start = new int[2];
+    end = new int[2];
+    start[0] = inf.nextInt();
+    start[1] = inf.nextInt();
+    end[0] = inf.nextInt();
+    end[1] = inf.nextInt();
   }
 
   public static int silver(String filename){
     return 0;
   }
+
 
 
 
